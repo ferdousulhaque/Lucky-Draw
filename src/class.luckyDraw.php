108e85da38c14e3263aa0750e3bb2da85beaf711
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2019-01-19 16:31:52 +0600

    Update class.luckyDraw.php

--- src/class.luckyDraw.php
+++ src/class.luckyDraw.php
@@ -1,6 +1,19 @@
 <?php
 class luckyDraw {
     public static function get(array $items) {
+        if(count($items)<1){
+            throw new \LengthException('Invalid number of items!');
+        }
+        foreach($items as $item){
+            if(!isset($item['item'])||!isset($item['chances'])||!isset($item['amounts'])){
+                throw new \InvalidArgumentException('Required keys(item,chances,amounts) not present with all items!');
+            } elseif(!is_int($item['chances'])||!is_array($item['amounts'])){
+                throw new \UnexpectedValueException('Chances should be an Integer value and amounts should be a formatted array!');
+            }
+        }
+        return self::gift($items);
+    }
+    private static function gift($items) {
         $chances = array_column($items,'chances','item');
         $item = self::generate($chances);
         $amounts = $items[array_search($item, array_column($items, 'item'))]['amounts'];
@@ -8,7 +21,7 @@ class luckyDraw {
         return [$item,$count];
     }
 
-    private static function generate(array $items) {
+    private static function generate($items) {
         if(count($items)==1) return $items[0];
         $rand = mt_rand(1, (int) array_sum($items));
         foreach ($items as $key => $value) {
